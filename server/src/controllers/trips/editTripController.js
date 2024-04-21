edcc6ef8f4e82ac2d088e956b168544444e3e12d
Fran <franciscojc1995@gmail.com>
2024-04-21 14:16:54 +0200

    Desactivar, Reactivar o confirmar viaje

---
+++ server/src/controllers/trips/editTripController.js
@@ -0,0 +1,52 @@
+import desactivarTripModel from '../../models/trips/desactivarTripModel.js';
+
+import reActivarTripModel from '../../models/trips/reActivarTripModel.js';
+
+import confirmarTripModel from '../../models/trips/confirmarTripModel.js';
+
+import validateSchema from '../../utils/validateSchemaUtil.js';
+
+import editTripSchema from '../../schemas/trips/editTripSchema.js';
+
+import { unauthorizedUserError } from '../../services/errorService.js';
+
+const newTripController = async (req, res, next) => {
+    try {
+        if (req.user.role !== 'admin') {
+            unauthorizedUserError();
+        }
+
+        await validateSchema(editTripSchema, req.body);
+
+        const { viajeId } = req.params;
+
+        const { desactivar, reactivar, confirmar } = req.body;
+
+        if (desactivar) {
+            await desactivarTripModel(viajeId);
+
+            res.status(201).send({
+                status: 'ok',
+                message: 'Viaje desactivado',
+            });
+        } else if (reactivar) {
+            await reActivarTripModel(viajeId);
+
+            res.status(201).send({
+                status: 'ok',
+                message: 'Viaje reactivado',
+            });
+        } else if (confirmar) {
+            await confirmarTripModel(viajeId);
+
+            res.status(201).send({
+                status: 'ok',
+                message: 'Viaje confirmado',
+            });
+        }
+    } catch (err) {
+        next(err);
+    }
+};
+
+export default newTripController;
--- server/src/routes/tripRoutes.js
+++ server/src/routes/tripRoutes.js
@@ -2,10 +2,14 @@ import express from 'express';
 
 import { newTripController } from '../controllers/trips/index.js';
 
+import { editTripController } from '../controllers/trips/index.js';
+
 import { authUserController } from '../middlewares/index.js';
 
 const router = express.Router();
 
 router.post('/viajes', authUserController, newTripController);
+
+router.post('/viajes/:viajeId', authUserController, editTripController);
 
 export default router;
