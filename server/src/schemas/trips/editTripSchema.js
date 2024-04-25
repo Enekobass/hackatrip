d23180344e88667170db9dbfc9270e57682419c5
Fran <franciscojc1995@gmail.com>
2024-04-25 18:49:06 +0200

    Arreglo de booleans

--- server/src/schemas/trips/editTripSchema.js
+++ server/src/schemas/trips/editTripSchema.js
@@ -3,9 +3,9 @@ import joi from 'joi';
 import joiErrorMessages from '../joiErrorMessages.js';
 
 const editTripSchema = joi.object({
-    desactivar: joi.string().optional().messages(joiErrorMessages),
-    reactivar: joi.string().optional().messages(joiErrorMessages),
-    confirmar: joi.string().optional().messages(joiErrorMessages),
+    desactivar: joi.bool().optional().messages(joiErrorMessages),
+    reactivar: joi.bool().optional().messages(joiErrorMessages),
+    confirmar: joi.bool().optional().messages(joiErrorMessages),
 });
 
 export default editTripSchema;
