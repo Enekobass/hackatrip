97f22b916f0760af321cf296df89210ff551111e
Fran <franciscojc1995@gmail.com>
2024-04-25 11:37:56 +0200

    Validacion con JOI de los endpoints

--- server/src/controllers/trips/addPhotoController.js
+++ server/src/controllers/trips/addPhotoController.js
@@ -1,11 +1,21 @@
 import insertPhotoModel from '../../models/trips/insertPhotoModels.js';
+
 import { savePhoto } from '../../services/photoService.js';
+
+import validateSchema from '../../utils/validateSchemaUtil.js';
+
+import addPhotoSchema from '../../schemas/trips/addPhotoSchema.js';
 
 const addPhotoController = async (req, res, next) => {
     try {
+        await validateSchema(addPhotoSchema, req.files);
+
         const { viajeId } = req.params;
+
         const photo = req.files;
+
         const photoName = await savePhoto(photo, 1000);
+
         const photoId = await insertPhotoModel(photoName, viajeId);
 
         res.status(201).send({
