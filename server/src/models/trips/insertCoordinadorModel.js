611d9b38e12ced7a7328eaa2437f50083a982259
Fran <franciscojc1995@gmail.com>
2024-05-04 14:47:43 +0200

    No permitir a un coordinador apuntarse mas de una vez al mismo viaje

--- server/src/models/trips/insertCoordinadorModel.js
+++ server/src/models/trips/insertCoordinadorModel.js
@@ -1,7 +1,18 @@
 import getPool from '../../db/getPool.js';
 
+import { cantBeCoordinador } from '../../services/errorService.js';
+
 const insertCoordinadorModel = async (coordinadorId, viajeId) => {
     const pool = await getPool();
+
+    const [coordinador] = await pool.query(
+        `SELECT userId FROM coordinadorviajes WHERE viajeId = ?`,
+        [viajeId],
+    );
+
+    if (coordinador[0]) {
+        cantBeCoordinador();
+    }
 
     await pool.query(
         `INSERT INTO coordinadorviajes (userId, viajeId) VALUES (?, ?)`,
