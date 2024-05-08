9080c1399e34e975cab8e7b93378fb76ba0a709e
Fran <franciscojc1995@gmail.com>
2024-05-08 19:08:55 +0200

    Arreglar votos de coordinadores

--- server/src/models/trips/insertCoordinadorVoteModel.js
+++ server/src/models/trips/insertCoordinadorVoteModel.js
@@ -1,11 +1,14 @@
 import getPool from '../../db/getPool.js';
 
-import { cantVoteCoordinador } from '../../services/errorService.js';
+import {
+    cantVoteCoordinador,
+    cantVoteCoordinadorAgain,
+} from '../../services/errorService.js';
 
 const insertCoordinadorVoteModel = async (value, viajeId, userId) => {
     const pool = await getPool();
 
-    const [user] = await pool.query(
+    let [user] = await pool.query(
         `SELECT userId FROM viajesreservados WHERE userId = ? AND viajeId = ?`,
         [userId, viajeId],
     );
@@ -19,9 +22,18 @@ const insertCoordinadorVoteModel = async (value, viajeId, userId) => {
         [viajeId],
     );
 
+    [user] = await pool.query(
+        `SELECT userId FROM coordinadorviajes WHERE viajeId = ?`,
+        [viajeId],
+    );
+
+    if (user) {
+        cantVoteCoordinadorAgain();
+    }
+
     await pool.query(
-        `INSERT INTO coordinadorvotes (value, userId, viajeId) VALUES (?, ?, ?)`,
-        [value, coordinadorId[0].userId, viajeId],
+        `INSERT INTO coordinadorvotes (value, coordinadorId, viajeId, userId) VALUES (?, ?, ?, ?)`,
+        [value, coordinadorId[0].userId, viajeId, userId],
     );
 };
 
