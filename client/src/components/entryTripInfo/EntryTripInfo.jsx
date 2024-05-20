586b04fe469e7a329c28e58cb4ab993049e9b41b
Brais Bernardez <braiseijan@gmail.com>
2024-05-20 11:06:59 +0200

    css entrytripinfo

--- client/src/components/entryTripInfo/EntryTripInfo.jsx
+++ client/src/components/entryTripInfo/EntryTripInfo.jsx
@@ -1,66 +1,72 @@
-import PropType from 'prop-types';
+import PropTypes from 'prop-types';
+import './EntryTripInfo.css';
 
 const { VITE_API_URL } = import.meta.env;
 
-const EntryTripInfo = ({ destino, titulo, descripcion, fechaDeInicio, fechaDeFin, plazasMinimas, plazasMaximas, ruta, precio, photo, activo, confirmado }) => {
+const TripInfo = ({ destino, titulo, descripcion, fechaDeInicio, fechaDeFin, plazasMinimas, plazasMaximas, ruta, precio, photo, activo, confirmado }) => {
+
     return (
-        <ul className='entry-details'>
-            <li>
+        <div>
+            <div className="entry-header">
                 <img
                     src={`${VITE_API_URL}/${photo}`}
                     alt={`Foto de ${destino}`}
-                    />
-            </li>
-            <li>
-                <strong>Lugar:</strong> {destino}
-            </li>
-            <li>
-                <strong>Título:</strong> {titulo}
-            </li>
-            <li>
-                <strong>Descripción:</strong> {descripcion}
-            </li>
-            <li>
-                <strong>Fecha de inicio:</strong> {fechaDeInicio}
-            </li>
-            <li>
-                <strong>Fecha de fin:</strong>{fechaDeFin}
-            </li>
-            <li>
-                <strong>Plazas mínimas:</strong>{plazasMinimas}
-            </li>
-            <li>
-                <strong>Plazas máximas:</strong> {plazasMaximas}
-            </li>
-            <li>
-                <strong>Ruta:</strong> {ruta}
-            </li>
-            <li>
-                <strong>Precio</strong> {precio}
-            </li>
-            <li>
-                <strong>Activo:</strong> {activo}
-            </li>
-            <li>
-                <strong>Confirmado:</strong> {confirmado}
-            </li>
-        </ul>
+                />
+                <div className="place-overlay">
+                    <strong>Lugar:</strong> {destino}
+                </div>
+                <div className="dates-overlay">
+                    <span className="fecha-in">{fechaDeInicio}</span>
+                    <span>-</span>
+                    <span className="fecha-fin">{fechaDeFin}</span>
+                </div>
+            </div>
+            <ul className='entry-details'>
+                <li className="title">
+                    <strong>Título:</strong> {titulo}
+                </li>
+                <li className="description">
+                    <strong>Descripción:</strong> {descripcion}
+                </li>
+                <li className="itinerary">
+                    <h2>Itinerario</h2>
+                    <div className="route">
+                        <strong>Ruta:</strong> {ruta}
+                    </div>
+                </li>
+                <li className="hidden">
+                    <strong>Plazas mínimas:</strong> {plazasMinimas}
+                </li>
+                <li className="hidden">
+                    <strong>Plazas máximas:</strong> {plazasMaximas}
+                </li>
+                <li>
+                    <strong>Precio:</strong> {precio}
+                </li>
+                <li>
+                    <strong>Activo:</strong> {activo}
+                </li>
+                <li>
+                    <strong>Confirmado:</strong> {confirmado}
+                </li>
+            </ul>
+        </div>
     );
 };
 
 EntryTripInfo.propTypes = {
-    destino: PropType.string.isRequired,
-    descripcion: PropType.string.isRequired,
-    titulo: PropType.string.isRequired,
-    fechaDeInicio: PropType.string.isRequired,
-    fechaDeFin: PropType.string.isRequired,
-    plazasMinimas: PropType.number.isRequired,
-    plazasMaximas: PropType.number.isRequired,
-    ruta: PropType.string.isRequired,
-    precio: PropType.number.isRequired,
-    photo: PropType.string.isRequired,
-    activo: PropType.number.isRequired,
-    confirmado: PropType.number.isRequired,
+    destino: PropTypes.string.isRequired,
+    descripcion: PropTypes.string.isRequired,
+    titulo: PropTypes.string.isRequired,
+    fechaDeInicio: PropTypes.string.isRequired,
+    fechaDeFin: PropTypes.string.isRequired,
+    plazasMinimas: PropTypes.number.isRequired,
+    plazasMaximas: PropTypes.number.isRequired,
+    ruta: PropTypes.string.isRequired,
+    precio: PropTypes.number.isRequired,
+    photo: PropTypes.string.isRequired,
+    activo: PropTypes.number.isRequired,
+    confirmado: PropTypes.number.isRequired,
 };
 
 export default EntryTripInfo;
