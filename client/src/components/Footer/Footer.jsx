520cf65c04329ccec6a8247982ced536181a548d
Brais Bernardez <braiseijan@gmail.com>
2024-05-27 16:35:42 +0200

    footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -12,11 +12,37 @@ const Footer = () => {
           />
         </div>
         <div className='footer__nav'>
-          <ul>
-            <li><a href='/about'>Sobre Nosotros</a></li>
-            <li><a href='/services'>Servicios</a></li>
+          <ul className='footer__nav-list'>
+            <li><a href='/'>Inicio</a></li>
+            <li><a href='/viaje/:viajeId'>Viajes</a></li>
+            <li className='footer__nav-item'>
+              <a href='/services'>Destinos</a>
+              <ul className='footer__destinations'>
+                <li><a href='/north-america'>América del Norte</a></li>
+                <li><a href='/latin-america'>Latinoamérica</a></li>
+                <li><a href='/africa'>África</a></li>
+                <li><a href='/middle-east'>Oriente Medio</a></li>
+                <li><a href='/asia'>Asia</a></li>
+                <li><a href='/europe'>Europa</a></li>
+                <li><a href='/northern-europe'>Norte de Europa</a></li>
+                <li><a href='/spain-portugal'>España y Portugal</a></li>
+              </ul>
+            </li>
+            <li className='footer__nav-item'>
+            <a className='sublista' href='/services'>Fechas</a>
+            <ul className='footer__destinations'>
+              <li><a href='/north-america'>Marzo</a></li>
+              <li><a href='/north-america'>Abril</a></li>
+              <li><a href='/latin-america'>Mayo</a></li>
+              <li><a href='/africa'>Junio</a></li>
+              <li><a href='/middle-east'>Julio</a></li>
+              <li><a href='/asia'>Agosto</a></li>
+              <li><a href='/europe'>Septiembre</a></li>
+              <li><a href='/northern-europe'>Octubre</a></li>
+            </ul>
+          </li>
+            <li><a href='/comunity'>Comunidad</a></li>
             <li><a href='/contact'>Contacto</a></li>
-            <li><a href='/privacy'>Política de Privacidad</a></li>
           </ul>
         </div>
         <div className='footer__contact'>
