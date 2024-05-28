cff0ff89e9440b3b0f845418a204f447f761cac7
Fran <franciscojc1995@gmail.com>
2024-05-28 16:43:12 +0200

    Arreglo de css en algunos filtros

--- client/src/components/TripListItem/TripListItem.jsx
+++ client/src/components/TripListItem/TripListItem.jsx
@@ -7,42 +7,53 @@ import "./TripListItem.css"
 
 const { VITE_API_URL } = import.meta.env;
 
+const formatDate = (dateString) => {
+  const options = { day: 'numeric', month: 'short' };
+  return new Date(dateString).toLocaleDateString('es-ES', options);
+};
+
+const today = new Date();
+
 function TripListItem(sixTrips, {loading}) {
 
   return (
     <Grid container justifyContent="center">
       {(loading ? Array.from(new Array(6)) : sixTrips.sixTrips.sixTrips).map((trip, index) => (
+        formatDate(trip.fechaDeInicio) > formatDate(today) ?
         <Box key={index} sx={{ width: 450, marginRight: 5, mt: 5 }}>
           {trip ? (
               <Link to={`/viaje/${trip.id}`} className='trip-item'>
-                <img
-                  style={{ width: 450, height: 350, border: '2px solid white', borderRadius: '20px' }}
-                  alt={trip.destino}
-                  src={`${VITE_API_URL}/${trip.imagen}`}
-                />
-              </Link>
+              <img
+                style={{ width: 450, height: 350, border: '2px solid white', borderRadius: '20px' }}
+                alt={trip.destino}
+                src={`${VITE_API_URL}/${trip.imagen}`}
+              />
+            </Link>
           ) : (
             <Skeleton variant="rectangular" width={210} height={118} />
           )}
 
           {trip ? (
-            <Link to={`/viaje/${trip.id}`} className='trip-item'>
-              <Box sx={{ pr: 2, marginLeft: 7}}>
-                <Typography gutterBottom variant="body1" color="ButtonText">
-                  {trip.destino}
-                </Typography>
-                <Typography display="block" variant="subtitle1" color="GrayText">
-                  {trip.precio}€
-                </Typography>
-              </Box>
-            </Link>
+            formatDate(trip.fechaDeInicio) > formatDate(today) ?
+              <Link to={`/viaje/${trip.id}`} className='trip-item'>
+                <Box sx={{ pr: 2, marginLeft: 7}}>
+                  <Typography gutterBottom variant="body1" color="ButtonText">
+                    {trip.destino}
+                  </Typography>
+                  <Typography display="block" variant="subtitle1" color="GrayText">
+                    {trip.precio}€
+                  </Typography>
+                </Box>
+              </Link>
+              :
+              console.log()
           ) : (
             <Box sx={{ pt: 0.5 }}>
               <Skeleton />
               <Skeleton width="60%" />
             </Box>
           )}
-        </Box>
+        </Box> : console.log()
       ))}
     </Grid>
   );
